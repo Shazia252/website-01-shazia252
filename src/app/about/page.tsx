a86f32c0eaa173424e7de960a8e750f70ff82b15
Shazia252 <161409243+Shazia252@users.noreply.github.com>
2024-11-25 12:43:04 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,8 +8,8 @@ const About = () => {
                 <h2 className='fade-in'>About Us</h2>
                 <p>Welcome to Awesome Watches
 
-Discover the perfect blend of style, precision, and craftsmanship with Awesome Watches. We offer an exclusive collection of timepieces designed for those who value elegance and functionality. Whether you're looking for a sleek modern design, a classic vintage style, or a rugged sports watch, we have something for everyone.
-Each watch in our collection is crafted with meticulous attention to detail, using high-quality materials to ensure durability and timeless appeal. Explore our categories, read reviews, and find the watch that fits your personality and lifestyle.Time waits for no one—start your journey to timeless style with Awesome Watches today!</p>
+Discover the perfect blend of style precision and craftsmanship with Awesome Watches. We offer an exclusive collection of timepieces designed for those who value elegance and functionality. Whether you&aposre looking for a sleek modern design a classic vintage style or a rugged sports watch we have something for everyone.
+Each watch in our collection is crafted with meticulous attention to detail using high-quality materials to ensure durability and timeless appeal. Explore our categories read reviews and find the watch that fits your personality and lifestyle.Time waits for no one—start your journey to timeless style with Awesome Watches today!</p>
             </div>
         </section>
       
